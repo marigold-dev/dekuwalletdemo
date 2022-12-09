bd3c267cc12b525d6e2d2a876392b77928938842
Benjamin Fuentes <benjamin.fuentes@marigold.dev>
2022-12-09 11:03:40 +0100

    update XTZ to CTEZ

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,22 @@
-import { BeaconMessageType, BlockchainRequestV3, BlockchainResponseV3, DAppClient, DekuBlockchain, DekuMessageType, DekuPermissionScope, DekuTransferRequest } from '@airgap/beacon-sdk';
-import { DekuToolkit, fromBeaconSigner } from '@marigold-dev/deku-toolkit';
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
-import ConnectButton from './ConnectWallet';
-import DisconnectButton from './DisconnectWallet';
+import {
+  BeaconMessageType,
+  BlockchainRequestV3,
+  BlockchainResponseV3,
+  DAppClient,
+  DekuBlockchain,
+  DekuMessageType,
+  DekuPermissionScope,
+  DekuTransferRequest,
+} from "@airgap/beacon-sdk";
+import { DekuToolkit, fromBeaconSigner } from "@marigold-dev/deku-toolkit";
+import { PackDataParams, PackDataResponse } from "@taquito/rpc";
+import { MichelCodecPacker } from "@taquito/taquito";
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import ConnectButton from "./ConnectWallet";
+import DisconnectButton from "./DisconnectWallet";
 
 function App() {
-
   const [dAppClient, setdAppClient] = useState<DAppClient>();
   const [userAddress, setUserAddress] = useState<string>("");
   let oldUserAddress = useRef<string>();
@@ -14,65 +24,93 @@ function App() {
   const [userBalance, setUserBalance] = useState<number>(0);
   const [userAddress2, setUserAddress2] = useState<string>("");
   const [dekuClient, setDekuClient] = useState<DekuToolkit>();
+  const [ticketBytes, setTicketBytes] = useState<string>("");
 
   const ticketer: string = process.env["REACT_APP_CONTRACT"]!;
-  const ticketBytes: string = "050505030b";
+
+  async function getFABytes(contractAddress: string): Promise<string> {
+    const p = new MichelCodecPacker();
+    let addrBytes: PackDataResponse = await p.packData({
+      data: { string: contractAddress }, // process.env["REACT_APP_CTEZ_CONTRACT"]!},
+      type: { prim: "address" },
+    });
+
+    //why to remove first 12 chars ? no idea but it is like this ...
+    //console.log("addrBytes",addrBytes.packed.substring(12));
+
+    let FAbytes: PackDataParams = {
+      data: {
+        prim: "Right",
+        args: [{ bytes: addrBytes.packed.substring(12) }],
+      }, //'01f37d4eddfff4e08fb1f19895ac9c83bc12d2b36800'}]},
+      type: {
+        prim: "Or",
+        args: [
+          { prim: "Unit", annots: ["%XTZ"] },
+          { prim: "address", annots: ["%FA"] },
+        ],
+      },
+    };
+    return (await p.packData(FAbytes)).packed;
+  }
 
   useEffect(() => {
     (async () => {
+      setTicketBytes(await getFABytes(process.env["REACT_APP_CTEZ_CONTRACT"]!));
 
       let client: DAppClient = new DAppClient({ name: "Test" });
       const dekuBlockchain = new DekuBlockchain();
       client.addBlockchain(dekuBlockchain);
       setdAppClient(client);
 
-
       const dekuClient = new DekuToolkit({
-        dekuRpc: process.env["REACT_APP_DEKU_NODE"]!, dekuSigner: fromBeaconSigner(dAppClient!)
-      })
-        .setTezosRpc(process.env["REACT_APP_TEZOS_NODE"]!)
-        .onBlock(block => {
-          console.log("The client received a block");
-          console.log(block);
-        });
-
+        dekuRpc: process.env["REACT_APP_DEKU_NODE"]!,
+        dekuSigner: fromBeaconSigner(dAppClient!),
+      }).setTezosRpc(process.env["REACT_APP_TEZOS_NODE"]!);
       setDekuClient(dekuClient);
 
       setUserAddress2("tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6");
       console.log("call once", dekuClient);
     })();
-  }, []
-  );
+  }, []);
 
   useEffect(() => {
     (async () => {
       if (dekuClient) {
         if (userAddress) {
-          setUserBalance(await dekuClient!.getBalance(userAddress, { ticketer: ticketer, data: ticketBytes }));
+          setUserBalance(
+            await dekuClient!.getBalance(userAddress, {
+              ticketer: ticketer,
+              data: ticketBytes,
+            })
+          );
           oldUserAddress.current = userAddress; //keep ref
           const intervalId = setInterval(async () => {
             try {
-              if (oldUserAddress.current) setUserBalance(await dekuClient!.getBalance(oldUserAddress.current, { ticketer: ticketer, data: ticketBytes }));
+              if (oldUserAddress.current)
+                setUserBalance(
+                  await dekuClient!.getBalance(oldUserAddress.current, {
+                    ticketer: ticketer,
+                    data: ticketBytes,
+                  })
+                );
               console.log("Balance refreshed on " + oldUserAddress.current);
             } catch (err) {
               console.log(err);
             }
           }, 15 * 1000);
-          return () => { clearInterval(intervalId); };
-
+          return () => {
+            clearInterval(intervalId);
+          };
         } else {
           oldUserAddress.current = undefined;
         }
-
       }
     })();
-
-  }, [userAddress]
-  );
+  }, [userAddress]);
 
   const handleL2Transfer = async () => {
     try {
-
       let request: DekuTransferRequest = {
         blockchainIdentifier: "deku",
         type: BeaconMessageType.BlockchainRequest,
@@ -80,16 +118,20 @@ function App() {
           type: DekuMessageType.transfer_request,
           scope: DekuPermissionScope.transfer,
           amount: "1",
-          mode: 'submit',
+          mode: "submit",
           recipient: userAddress2,
           sourceAddress: userAddress,
           ticketer: ticketer,
           data: ticketBytes,
-          options: {}
-        }
+          options: {},
+        },
       };
 
-      const br: BlockchainResponseV3<string> | undefined = await dAppClient?.request({ ...request, accountId: (await dAppClient.getActiveAccount())!.accountIdentifier } as BlockchainRequestV3<"deku">);
+      const br: BlockchainResponseV3<string> | undefined =
+        await dAppClient?.request({
+          ...request,
+          accountId: (await dAppClient.getActiveAccount())!.accountIdentifier,
+        } as BlockchainRequestV3<"deku">);
       console.log("BlockchainResponseV3", br);
       // const opHash = await dekuClient!.transferTo(userAddress2, 1, ticketer, ticketBytes);
     } catch (error: any) {
@@ -101,33 +143,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <h1>DEKU DEMO</h1>
         <h2>Sign tx with Temple/BeaconSDK v3 messages</h2>
 
         <hr style={{ width: "100%" }} />
 
-        {!userAddress ?
-          <ConnectButton
-            Tezos={dAppClient!}
-            setUserAddress={setUserAddress}
-          />
-          :
+        {!userAddress ? (
+          <ConnectButton Tezos={dAppClient!} setUserAddress={setUserAddress} />
+        ) : (
           <DisconnectButton
             Tezos={dAppClient!}
             userAddress={userAddress}
             setUserAddress={setUserAddress}
             setUserBalance={setUserBalance}
-          />}
+          />
+        )}
 
         <div>
-          I am {userAddress} with {userBalance} XTZ tickets
+          I am {userAddress} with {userBalance} CTEZ-tickets ({ticketBytes})
         </div>
 
         <br />
         <div>
           Enter L2 address here :
-          <input value={userAddress2} onChange={(e) => setUserAddress2(e.currentTarget.value)}></input>
+          <input
+            value={userAddress2}
+            onChange={(e) => setUserAddress2(e.currentTarget.value)}
+          ></input>
           <button onClick={handleL2Transfer}>Send money</button>
         </div>
       </header>
